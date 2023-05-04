fc97dcd0e2ee79984ceb0f92ebbad2d37b4ad657
Denis Babaev <babaev.d@ctrl2go.solutions>
2023-05-04 21:28:55 +0500

    (technical-task) Add test file API

--- api/urls.py
+++ api/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import TestRunRequestAPIView, TestRunRequestItemAPIView, AssetsAPIView
+from .views import TestRunRequestAPIView, TestRunRequestItemAPIView, AssetsAPIView, TestFileUploadRequestAPIView
 
 urlpatterns = [
     path('assets', AssetsAPIView.as_view(), name='assets'),
+    path('test-file', TestFileUploadRequestAPIView.as_view(), name='test_file_upload_req'),
     path('test-run', TestRunRequestAPIView.as_view(), name='test_run_req'),
     path('test-run/<pk>', TestRunRequestItemAPIView.as_view(), name='test_run_req_item'),
 ]
