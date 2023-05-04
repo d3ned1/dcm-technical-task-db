8dead2623cf76a0c6ac8765b9f90a1ef4bb20aaa
Denis Babaev <babaev.d@ctrl2go.solutions>
2023-05-04 21:29:29 +0500

    (technical-task) Add upload directory, test file upload serializers

--- api/serializers.py
+++ api/serializers.py
@@ -1,6 +1,9 @@
+from pathlib import Path
+
 from rest_framework import serializers
 
-from api.models import TestRunRequest, TestFilePath, TestEnvironment
+from api.models import TestRunRequest, TestFilePath, TestEnvironment, TestUploadDirectory
+from ionos.settings import BASE_DIR
 
 
 class TestRunRequestSerializer(serializers.ModelSerializer):
@@ -49,7 +52,39 @@ class TestFilePathSerializer(serializers.ModelSerializer):
         fields = ('id', 'path')
 
 
+class TestFileUploadRequestSerializer(serializers.Serializer):
+    test_file = serializers.FileField()
+    upload_dir = serializers.CharField(max_length=1024)
+
+    def create(self, validated_data):
+        """
+        Extract uploaded file from corresponding field.
+        Check that directory already exists.
+        Save file on disk or overwrite if already exists.
+        """
+        test_file = validated_data["test_file"]
+        upload_dir = validated_data["upload_dir"]
+        directory = Path(BASE_DIR) / upload_dir
+
+        if not directory.exists():
+            directory.mkdir()
+
+        with open(Path(BASE_DIR) / upload_dir / test_file.name, "wb+") as destination:
+            for chunk in test_file.chunks():
+                destination.write(chunk)
+        return validated_data
+
+    def update(self, instance, validated_data):
+        raise Exception("Update method is not allowed")
+
+
 class TestEnvironmentSerializer(serializers.ModelSerializer):
     class Meta:
         model = TestEnvironment
         fields = ('id', 'name')
+
+
+class TestUploadDirectorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = TestUploadDirectory
+        fields = ('id', 'directory')
